f4b50e4eeca5e62f51ffe202b215d00fe753f63b
David Morrison <drmorr@appliedcomputing.io>
2024-06-24 15:24:46 -0700

    use secrets_from

--- k8s/main.py
+++ k8s/main.py
@@ -10,28 +10,27 @@ DIFF_FILENAME = "k8s.df"
 SERVER_PORT = 1234
 
 
-class Prom2Parquet(fire.AppPackage):
+class Prom2parquet(fire.AppPackage):
     def __init__(self):
         try:
-            with open(os.getenv('BUILD_DIR') + f'/{self.id}-image') as f:
+            with open(os.getenv('BUILD_DIR') + f'/{self.id()}-image') as f:
                 image = f.read()
         except FileNotFoundError:
             image = 'PLACEHOLDER'
 
         env = (fire.EnvBuilder({"AWS_DEFAULT_REGION": "us-east-1"})
-            .with_secret("AWS_ACCESS_KEY_ID", "simkube", "aws_access_key_id")
-            .with_secret("AWS_SECRET_ACCESS_KEY", "simkube", "aws_secret_access_key")
+            .with_secrets_from("simkube")
         )
 
         container = fire.ContainerBuilder(
-            name=self.id,
+            name=self.id(),
             image=image,
             args=[
                 "/prom2parquet",
             ],
         ).with_env(env).with_ports(SERVER_PORT).with_security_context(Capability.DEBUG)
 
-        self._depl = (fire.DeploymentBuilder(app_label=self.id)
+        self._depl = (fire.DeploymentBuilder(app_label=self.id())
             .with_containers(container)
             .with_service()
             .with_node_selector("type", "kind-worker")
@@ -40,15 +39,12 @@ class Prom2Parquet(fire.AppPackage):
     def compile(self, chart: Construct):
         self._depl.build(chart)
 
-    @property
-    def id(self) -> str:
-        return "prom2parquet"
 
 if __name__ == "__main__":
     dag_path = f"{os.getenv('BUILD_DIR')}/{DAG_FILENAME}"
     diff_path = f"{os.getenv('BUILD_DIR')}/{DIFF_FILENAME}"
     graph, diff = fire.compile({
-        "monitoring": [Prom2Parquet()],
+        "monitoring": [Prom2parquet()],
     }, dag_path)
 
     with open(dag_path, "w") as f:
